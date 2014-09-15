580839851da82475cde43a0cf594ff2bd7206513
DevilXu <a630975128@126.com>
2014-09-15 17:03:18 +0800

    v0.3

---
+++ src/org/common/base/Util.java
@@ -0,0 +1,46 @@
+package org.common.base;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Util {
+	/**
+	 * 对象序列话
+	 * @param object
+	 * @return
+	 */
+    public static byte[] serialize(Object object) {
+    	ObjectOutputStream oos = null;
+    	ByteArrayOutputStream baos = null;
+    	try {
+    		//序列化
+    		baos = new ByteArrayOutputStream();
+    		oos = new ObjectOutputStream(baos);
+    		oos.writeObject(object);
+    		byte[] bytes = baos.toByteArray();
+    		return bytes;
+    	} catch (Exception e) {
+    	 
+    	}
+    	return null;
+    }
+    /**
+     * 反序列化
+     * @param bytes
+     * @return
+     */
+    public static Object unserialize(byte[] bytes) {
+    	ByteArrayInputStream bais = null;
+    	try {
+    		//反序列化
+    		bais = new ByteArrayInputStream(bytes);
+    		ObjectInputStream ois = new ObjectInputStream(bais);
+    		return ois.readObject();
+    	} catch (Exception e) {
+    	 
+    	}
+    	return null;
+    }
+}
